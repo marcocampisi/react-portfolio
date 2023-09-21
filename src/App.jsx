aa878295a7f40b41d42b34f0708f69e0fe884b54
marcocampisi <marco.campisi@outlook.it>
2023-09-21 18:50:46 +0200

    Fix app content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import HeaderComponent from "./components/HeaderComponent";
 import "./App.css";
+import MainComponent from "./components/MainComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
     return (
         <div>
             <HeaderComponent />
-            <main>
-                <div className='w-full text-center font-mono'>
-                    <h1 className='text-5xl font-extrabold my-5'>Marco Campisi</h1>
-                    <h3 className='text-2xl font-medium mt-4'>Questo è il mio portfolio.</h3>
-                </div>
-            </main>
+            <MainComponent />
+            <FooterComponent />
         </div>
     );
 }
