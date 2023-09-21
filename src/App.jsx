ac4d2838dc47dd32da1ac728c0b0eb834af24399
marcocampisi <marco.campisi@outlook.it>
2023-09-21 18:00:54 +0200

    Fix spaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import HeaderComponent from "./components/HeaderComponent";
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <div>
+            <HeaderComponent />
+            <main>
+                <div className="container w-full text-center font-mono">
+                    <h1 className="text-5xl font-extrabold">Marco Campisi</h1>
+                </div>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HeaderComponent extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>Header</h1>
+            </header>
+        );
+    }
+}
+
+export default HeaderComponent;
