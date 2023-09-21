27f6b3df4b2a1ab04812bdb0bc4aa3cc86d4a6f0
marcocampisi <marco.campisi@outlook.it>
2023-09-21 18:39:38 +0200

    Add base components

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
         <div>
             <HeaderComponent />
             <main>
-                <div className="container w-full text-center font-mono">
-                    <h1 className="text-5xl font-extrabold">Marco Campisi</h1>
+                <div className='w-full text-center font-mono'>
+                    <h1 className='text-5xl font-extrabold my-5'>Marco Campisi</h1>
+                    <h3 className='text-2xl font-medium mt-4'>Questo è il mio portfolio.</h3>
                 </div>
             </main>
         </div>
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 class HeaderComponent extends React.Component {
     render() {
         return (
             <header>
-                <h1>Header</h1>
+                <nav className='nav'>
+                    
+                </nav>
             </header>
         );
     }
