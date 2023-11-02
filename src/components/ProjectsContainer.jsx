fd92d5f9e5c11f03bcee26fb883bdf8af4469b4f
Marco Campisi <marco.campisi@outlook.it>
2023-11-02 19:41:56 +0100

    Removed link icon

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,6 +1,4 @@
 /* eslint-disable react/prop-types */
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from '@fortawesome/free-solid-svg-icons';
 function ProjectsContainer({ projects }) {
     return (
         <div className='container mx-auto mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
@@ -24,7 +22,6 @@ function Projects({ title, githubLink, description, thumbnail }) {
                 <h2 className='text-2xl font-bold mb-2'>{title}</h2>
                 <a href={githubLink} className="group relative">
                     <img src={thumbnail} alt="" className="my-4 h-[150px] w-full object-cover hover:scale-105 hover:grayscale transition cursor-pointer"/>
-                    <FontAwesomeIcon icon={faLink} className='h-8 m-4 opacity-50 group-hover:opacity-100 absolute bottom-0 left-0 transition'/>
                 </a>
                 <p>{description}</p>
             </div>
